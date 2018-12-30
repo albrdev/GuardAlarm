f88e1275ee82c99b7d3a7df3b0468195be15bfbe
albrdev <albrdev@gmail.com>
2018-12-31 00:54:35 +0100

    testing communication between server/client

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -14,60 +14,142 @@
 #include "packet.h"
 #include "misc.hpp"
 
-char portID[] = "\\\\.\\COM10";
+char portID[] = "COM10";
 char *portName = portID;
 unsigned int POLL = 100;
 
 char data[MAX_DATA_LENGTH] = { 0 };
-SerialPort serialPort(portName);
+SerialPort serialPort;
+
+const std::string usersFilePath = "users.dat";
+const std::string logFilePath = "system.log";
+const std::string sensorsFilePath = "sensors.dat";
+
+UserTable users;
+LogTable logs;
+SensorTable sensors;
+Logger logger;
 
 int main(void)
 {
-    printf("Connection %s\n", serialPort.IsConnected() ? "successfull" : "failed");
-    fflush(stdout);
-
-    //while(!serialPort.isConnected());
-
-    //int size = serialPort.readSerialPort(data, MAX_DATA_LENGTH);
-    while(serialPort.IsConnected())
+    if(!SensorTable::Load(sensorsFilePath, sensors))
     {
-        int size = serialPort.Read(data, sizeof(data));
-        if(size < (int)sizeof(packet_header_t))
+        fprintf(stderr, "*** Error reading sensors: %s:%zu\n", sensorsFilePath.c_str(), sensors.Count() + 1);
+        return 5;
+    }
+
+    printf("Sensors successfully loaded %zu entries\n", sensors.Count());
+    //printTable(sensors);
+
+    if(!UserTable::Load(usersFilePath, users))
+    {
+        fprintf(stderr, "*** Error reading users: %s:%zu\n", usersFilePath.c_str(), users.Count() + 1);
+        return 3;
+    }
+
+    printf("Users successfully loaded %zu entries\n", users.Count());
+    //printTable(users);
+
+    while(true)
+    {
+        while(!serialPort.Open(portName))
         {
-            if(size != 0)
+            fprintf(stderr, "*** Error: Connection failed on serial port \'%s\'\n", portName);
+            fflush(stderr);
+            Sleep(1000);
+        }
+
+        fprintf(stderr, "*** Connection successfully established on serial port \'%s\'\n", portName);
+        fflush(stderr);
+        Sleep(1000);
+
+        while(serialPort.IsConnected())
+        {
+            int size = serialPort.Read(data, sizeof(data));
+            if(size < (int)sizeof(packet_header_t))
             {
-                fprintf(stderr, "Error: Invalid packet size\n");
-                fflush(stderr);
+                if(size != 0)
+                {
+                    fprintf(stderr, "Error: Invalid packet size\n");
+                    fflush(stderr);
+                }
+
+                Sleep(POLL);
+                continue;
+            }
+
+            packet_header_t *pkt = (packet_header_t *)data;
+            uint16_t crc = mkcrc16((uint8_t *)pkt + sizeof(pkt->checksum), size - sizeof(pkt->checksum));
+
+            printf("recv(size=%d, crc=%04hX)\n", size, crc);
+            printf("header(chk=%04hX, type=%d)\n", pkt->checksum, pkt->type);
+            if(!packet_verify(pkt, size, pkt->checksum))
+            {
+                Sleep(POLL);
+                continue;
+            }
+
+            packet_pincode_t *dta;
+            switch(pkt->type)
+            {
+                case PT_PIN:
+                    dta = (packet_pincode_t *)data;
+                    printf("data(pin=%s, mode=%c (%hhu))\n", dta->pin, dta->mode, dta->mode);
+
+                    std::string providedPassword((const char *)dta->pin);
+                    UserEntry* userEntry = NULL;
+                    int statusFlags = userLogin(providedPassword, users, userEntry);
+                    if((statusFlags & AuthStatus::Success) != 0) // Check if AS_SUCCESS bit is set with AND
+                    {
+                        fprintf(stderr, "*** Successfull login: id=%d, name=%s, tag=%d, status=%d\n", userEntry->GetID(), userEntry->GetUsername().c_str(), userEntry->GetTagID(), userEntry->GetStatus());
+                        //attemptedLogins = 0; // Successfull login, clear login attempts
+                        //isAlarmed = false; // Turn off alarm
+
+                        if((statusFlags & AuthStatus::Emergency) != 0) // Check if AS_EMERGENCY bit is also set
+                        {
+                            // Call emergency function (this function should not print or alert somehow, just completly quiet, but not in this very test case)
+                            logger.WriteCSV(LogEntry(time(NULL), userEntry->GetID(), "Emergency code entered: Notifying the authorities!"));
+                            //emergencyResponse();
+                            // Continue as usual with the menu (to not startle the possible attacker(s))
+                        }
+                        else
+                        {
+                            packet_header_t pp;
+                            packet_mkheader(&pp, sizeof(pp), PT_SUCCESS);
+                            serialPort.Write((const char *)&pp, sizeof(pp));
+                        }
+
+                        //userSession(userEntry, isAlarmed, logger);
+                    }
+                    else
+                    {
+                        packet_header_t pp;
+                        packet_mkheader(&pp, sizeof(pp), PT_ERROR);
+                        serialPort.Write((const char *)&pp, sizeof(pp));
+                        // Login failed, count a maximum of LOGINATTEMPTS_MAX times in a row and then block and deactivate user
+                        logger.WriteCSV(LogEntry(time(NULL), -1, "Failed login attempt"));
+                        fprintf(stderr, "*** Error: Failed login attempt\n");
+                        /*if(++attemptedLogins >= LOGINATTEMPTS_MAX)
+                        {
+                            fprintf(stderr, "*** Warning: You have been locked out\n");
+                            if(userEntry != NULL) // Could be 'NULL' here (If login failed because we didn't find a user with that name)
+                            {
+                                userEntry->SetStatus(UserStatus::Blocked); // Deactivate the account
+                            }
+
+                            return -1; // Program exits and could (in the future) save timestamp somewhere safe, to use and refuse to start again under a certain time (to protect from further password-cracking attempts)
+                        }*/
+                    }
+
+                    putchar('\n');
+                    fflush(stdout);
+                    break;
             }
 
             Sleep(POLL);
-            continue;
         }
 
-        packet_header_t *pkt = (packet_header_t *)data;
-        uint16_t crc = mkcrc16((uint8_t *)pkt + sizeof(pkt->checksum), size - sizeof(pkt->checksum));
-
-        printf("recv(size=%d, crc=%04hX)", size, crc);
-        printf(", header(chk=%04hX, type=%d)", pkt->checksum, pkt->type);
-        if(!packet_verify(pkt, size, pkt->checksum))
-        {
-            Sleep(POLL);
-            continue;
-        }
-
-        packet_pincode_t *dta;
-        switch(pkt->type)
-        {
-            case PT_PIN:
-                dta = (packet_pincode_t *)data;
-                printf(", data(pin=%s, mode=%c (%hhu))\n", dta->pin, dta->mode, dta->mode);
-                break;
-        }
-
-        putchar('\n');
-        fflush(stdout);
-
-        Sleep(POLL);
+        fprintf(stderr, "*** Error: Lost serial connection on port \'%s\'\n", portName);
     }
 
     return 0;
